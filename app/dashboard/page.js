4af852ee2e8db810808560b1625c401c4b08237f
dav15jr <dnwanderajr@outlook.com>
2024-08-28 11:48:54 +0200

    First Commit

---
+++ app/dashboard/page.js
@@ -0,0 +1,25 @@
+import Dashboard from "@/components/Dashboard";
+import Login from "@/components/Login";
+import Main from "@/components/Main";
+
+export const metadata = {
+    title: "Motiv8r | Dashboard"
+  };
+
+
+export default function DashboardPage() {
+
+    const isAuthenticated = true;
+
+    let children = ( <Login />)
+
+        if (isAuthenticated) {
+        children = <Dashboard />
+        }
+
+    return (
+        <Main>
+            {children}
+        </Main>
+    )
+}

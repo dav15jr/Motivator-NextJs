d4d7aae2003755ce2cb1fde896d198713ea067d5
dav15jr <dnwanderajr@outlook.com>
2024-09-02 21:09:38 +0200

    Add database and Auth.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,10 @@
 import { Fugaz_One, Open_Sans } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import { AuthProvider } from "@/context/AuthContext";
+import Head from "./head";
+import Logout from "@/components/Logout";
+
 
 const opensans = Open_Sans({ subsets: ["latin"] });
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] });
@@ -13,25 +18,30 @@ export default function RootLayout({ children }) {
 
 const header  = (
   <header className='p-4 sm:p-8 flex items-center justify-between gap-4 '>
-    <h1 className={'text-base sm:text-lg textGradient ' + fugaz.className}>Motiv8r</h1>
-    <div className='flex items-center justify-between'>Place Holder</div>
+    <Link href={'/'}>
+      <h1 className={'text-base sm:text-lg textGradient ' + fugaz.className}>Motiv8r</h1>
+    </Link>
+    <Logout />
   </header>
 )
 const footer  = (
   <footer className='p-4 sm:p-8 grid place-items-center'>
-    <p className={'text-purple-500 ' + fugaz.className}>Created with 💪</p>
+    <Link href={'https://davisjr.netlify.app/'} target="_blank" className="">
+        <p className={'text-purple-500 duration-200 hover:text-white hover:bg-purple-500  ' + fugaz.className}>Created by D.Jr 💪</p>
+      </Link>
   </footer>
 )
 
-
-
   return (
     <html lang="en">
-      <body className={ 'w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col text-slate-800 ' + opensans.className}>
-        {header}
-        {children}
-        {footer}
-      </body>
+      <Head />
+      <AuthProvider>
+        <body className={ 'w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col text-slate-800 ' + opensans.className}>
+          {header}
+          {children}
+          {footer}
+        </body>
+      </AuthProvider>
     </html>
   );
 }
--- components/Hero.js
+++ components/Hero.js
@@ -1,21 +1,18 @@
 import { Fugaz_One } from 'next/font/google';
 import React from 'react'
-import Button from './Button';
-import Calender from './Calender';
+import Calendar from './Calendar';
+import CallToAction from './CallToAction';
 
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] });
 
 export default function Hero() {
   return (
-    <div className='py-4 md:py-10 flex flex-col gap-4 sm:gap-8'>
+    <div className='py-4 md:py-10 flex flex-col gap-8 sm:gap-10'>
         <h1 className={'text-5xl sm:text-6xl md:text-7xl text-center ' + fugaz.className}>
             <span className='textGradient'>Motiv8r </span>helps you track your <span className='textGradient'>daily </span>motivation!</h1>
-            <p className='text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[500px]'>Create your motivation record and see how you feel <span className='font-semibold'>on every day of every year.</span></p>
-        <div className='grid grid-cols-2 gap-4 w-fit mx-auto'>
-            <Button text="Sign Up" />
-            <Button text="Login" dark />
-        </div>
-            <Calender />
+            <p className='text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px]'>Create your motivation record and see how you feel <span className='font-semibold'>on every day of every year.</span></p>
+            <CallToAction />
+            <Calendar demo/>
     </div>
   )
 }
